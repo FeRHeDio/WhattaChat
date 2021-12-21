8cb078d1df76e01cdd3ecdbdb5e9f6cee4d6435a
Fernando Putallaz <fernando.putallaz@foxbox.com>
2021-12-21 19:01:56 -0300

    Initial Commit

---
+++ WhattaChat/WhattaChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhattaChatApp.swift
+//  WhattaChat
+//
+//  Created by Fernando Putallaz on 21/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct WhattaChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
