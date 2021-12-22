e4d36af9d551e17f5635adf1e50f8f41f80a0b83
Fernando Putallaz <fernando.putallaz@foxbox.com>
2021-12-22 10:13:38 -0300

    Add more noise to the conversation

--- WhattaChat/Chat.swift
+++ WhattaChat/Chat.swift
@@ -45,7 +45,24 @@ extension Chat {
     static let sampleData = [
         Chat(person: Person(name: "Charlie Bucket", image: "Ellipse-13"), messages: [
             Message("Hello there", type: .Sent, date: Date(timeIntervalSinceNow: -86450 * 3))
-        ], hasUnreadMessage: true)
+        ], hasUnreadMessage: true),
+    
+        Chat(person: Person(name: "Emilie Lost", image: "Ellipse-21"), messages: [
+            Message("Hi, I'm Emilie. I like to eat tomatoes", type: .Sent, date: Date(timeIntervalSinceNow: -86460 * 3))
+        ], hasUnreadMessage: false),
         
+        Chat(person: Person(name: "Ying Soduku", image: "Ellipse-36"), messages: [
+            Message("You have to regret about what you just said", type: .Sent, date: Date(timeIntervalSinceNow: -86320 * 3)),
+            Message("Because this are offending words", type: .Sent, date: Date(timeIntervalSinceNow: -86321 * 3)),
+            Message("You know what I'm talking about buddy?", type: .Sent, date: Date(timeIntervalSinceNow: -86321 * 3))
+        ], hasUnreadMessage: true),
+        
+        Chat(person: Person(name: "Manuelle Bored", image: "Ellipse-38"), messages: [
+            Message("This isn't funny at all", type: .Sent, date: Date(timeIntervalSinceNow: -86460 * 3))
+        ], hasUnreadMessage: false),
+    
+        Chat(person: Person(name: "Julia Kingston", image: "Ellipse-46"), messages: [
+            Message("Hey you never replied my messages, there is something wrong with you???", type: .Sent, date: Date(timeIntervalSinceNow: -86464 * 3))
+        ], hasUnreadMessage: true),
     ]
 }
