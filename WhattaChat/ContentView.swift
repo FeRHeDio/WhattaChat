204109fcdd2345e9f05f24b5e9ebb9f51484057a
Fernando Putallaz <fernando.putallaz@foxbox.com>
2021-12-22 12:09:06 -0300

    Added .serchable + iOS 15 as a minimum target

--- WhattaChat/ContentView.swift
+++ WhattaChat/ContentView.swift
@@ -10,14 +10,17 @@ import SwiftUI
 struct ContentView: View {
     let chats = Chat.sampleData
     
+    @State private var query = ""
+    
     var body: some View {
         NavigationView {
             List {
                 ForEach(chats) { chat in
                     ChatRow(chat: chat)
                 }
-            }
+            } 
             .listStyle(PlainListStyle())
+            .searchable(text: $query)
             .navigationTitle("Whatta Chat!")
             .navigationBarItems(trailing: Button(action: {}) {
                 Image(systemName: "square.and.pencil")
