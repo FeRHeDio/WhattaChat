dd3a5e9353770ca499bdb53d6e1a44c888928ebf
Fernando Putallaz <fernando.putallaz@foxbox.com>
2022-01-03 17:11:51 -0300

    Add some styles to the ChatView

--- WhattaChat/ChatView.swift
+++ WhattaChat/ChatView.swift
@@ -14,7 +14,12 @@ struct ChatView: View {
     let chat: Chat
     var body: some View {
         VStack(spacing: 0) {
-            Text("Something")
+            GeometryReader { reader in
+                ScrollView {
+                    getMessagesView(viewWidth: reader.size.width)
+                }
+            }
+            .background(.yellow)
         }
         .padding(.top, 1)
         .navigationBarTitleDisplayMode(.inline)
@@ -22,10 +27,36 @@ struct ChatView: View {
             chatsViewModel.markAsUnread(false, chat: chat)
         }
     }
+    
+    let columns = [GridItem(.flexible(minimum: 10))]
+    
+    func getMessagesView(viewWidth: CGFloat) -> some View {
+        LazyVGrid(columns: columns, spacing: 0) {
+            ForEach(chat.messages) { message in
+                let isReceived = message.type == .Received
+                HStack {
+                    ZStack {
+                        Text(message.text)
+                            .padding(.horizontal)
+                            .padding(.vertical, 12)
+                            .background(isReceived ? .black.opacity(0.2) : .green.opacity(0.9))
+                            .cornerRadius(13)
+                            
+                    }
+                    .frame(width: viewWidth * 0.7, alignment: isReceived ? .leading : .trailing)
+                    .padding(.vertical)
+//                    .background(.blue)
+                }
+                .frame(maxWidth: .infinity, alignment: isReceived ? .leading : .trailing)
+                .id(message.id)
+            }
+        }
+    }
 }
 
 struct ChatView_Previews: PreviewProvider {
     static var previews: some View {
-        ChatView(chat: Chat.sampleData[6])
+        ChatView(chat: Chat.sampleData[2])
+            .environmentObject(ChatsViewModel())
     }
 }
